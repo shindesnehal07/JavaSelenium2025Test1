c9fc7982b57bdd26c987481ea506d48fcceec752
shindesnehal07 <shindesnehal07@gmail.com>
2025-07-20 08:39:02 +0530

    project upload

---
+++ src/p1/Class1.java
@@ -0,0 +1,8 @@
+package p1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("P1 c1 world");
+	}
+}
---
+++ src/p2/Class2.java
@@ -0,0 +1,8 @@
+package p2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("P2 c2 world");
+	}
+}
